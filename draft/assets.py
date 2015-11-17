43ead35ba73e62e1457a42aba5d4016b03d9af03
Lucas Jenss <lucas@x3ro.de>
2015-11-17 09:42:14 +0000

    No more JS needed right no

--- draft/assets.py
+++ draft/assets.py
@@ -3,9 +3,5 @@ from flask.ext.assets import Environment, Bundle
 def setup(app):
     assets = Environment(app)
 
-    js = Bundle('js/jquery-2.1.4.js', 'js/draft.js', 'js/embed_gist.js',
-                filters='jsmin', output='js/packed.js')
-    assets.register('js_all', js)
-
     scss = Bundle('../sass/pixyll.scss', filters=['libsass', 'cssmin'], output='css/packed.css')
     assets.register('scss_all', scss)
