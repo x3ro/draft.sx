727f56fce1235e0435efcb1af94d046ca5afcf26
Lucas Jenss <lucas@x3ro.de>
2016-07-05 00:03:02 +0200

    Convert unicode emojis to SVG representation

--- draft/markup.py
+++ draft/markup.py
@@ -1,4 +1,6 @@
 import emoji
+import emoji_unicode
+import re
 
 from markdown import markdown
 from . import app
@@ -23,6 +25,8 @@ ALLOWED_ATTRIBUTES = {
 EXTENSIONS = ['attr_list', 'fenced_code', 'codehilite', 'def_list', 'footnotes',
               'tables', 'admonition']
 
+EMOJI_PATTERN = re.compile(emoji_unicode.RE_PATTERN_TEMPLATE)
+
 def render(content):
     """
     Convert given markup content to HTML.
@@ -36,6 +40,18 @@ def render(content):
     if content['language'] in RENDERABLE:
         app.logger.debug('{}: renderable!'.format(content['filename']))
         content['rendered'] = emoji.emojize(content['content'], use_aliases=True)
+        content['rendered'] = re.sub(EMOJI_PATTERN, emoji_match_handler, content['rendered'])
         content['rendered'] = markdown(content['rendered'], extensions=EXTENSIONS)
 
     return content
+
+def emoji_match_handler(m):
+    """
+    Matches unicode emoji and translates them to their SVG representation.
+    See https://github.com/nitely/emoji-unicode for more info
+    """
+    e = emoji_unicode.Emoji(unicode=m.group('emoji'))
+    return u'<img src="static/img/emoji/{filename}.svg" alt="{raw}" class="emoji">'.format(
+        filename=e.code_points,
+        raw=e.unicode
+    )
